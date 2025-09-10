a127f12d5e26cf36b23eea08fe1af13b8e52ebe2
AxelOsika <osikaaxel@gmail.com>
2025-09-10 23:34:52 +0200

    Bump to 0.1.0-og1; regenerate Autotools (configure, libtool m4, build-config)

--- build-config.h
+++ build-config.h
@@ -62,7 +62,7 @@
 #define PACKAGE_NAME "gpredict"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "gpredict 2.5.0-OGpredict"
+#define PACKAGE_STRING "gpredict 0.1.0-og1-dirty"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "gpredict"
@@ -71,7 +71,7 @@
 #define PACKAGE_URL ""
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "2.5.0-OGpredict"
+#define PACKAGE_VERSION "0.1.0-og1-dirty"
 
 /* Define to 1 if all of the C90 standard headers exist (not just the ones
    required in a freestanding environment). This macro is provided for
@@ -79,4 +79,4 @@
 #define STDC_HEADERS 1
 
 /* Version number of package */
-#define VERSION "2.5.0-OGpredict"
+#define VERSION "0.1.0-og1-dirty"
