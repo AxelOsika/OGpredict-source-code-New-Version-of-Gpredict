7c59a1c57a272853565fb17eaeb287828042ccfb
AxelOsika <osikaaxel@gmail.com>
2025-09-11 00:17:06 +0200

    Make main an index branch; code lives on source/windows/macos/linux

--- src/countries.h
+++
@@ -1,9 +0,0 @@
-#ifndef COUNTRIES_H
-#define COUNTRIES_H
-
-#include <gtk/gtk.h>
-
-// Fill a GtkListStore with your country names.
-void populate_country_liststore(GtkListStore *store);
-
-#endif // COUNTRIES_H
